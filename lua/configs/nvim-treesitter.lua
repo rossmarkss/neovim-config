1f2d005b3688764916bae5c2a2ebaca0fbdc117b
ross marks <rossmarks@proton.me>
2023-08-09 06:04:22 +0300

    upd treesitter config

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,4 +1,5 @@
 require('nvim-treesitter.configs').setup {
+    ensure_installed = { "c", "cpp", "rust", "python", "lua", "vim", "vimdoc", "cmake", "meson", "ninja", "make", "markdown", "markdown_inline", "llvm" },
     highlight = {
         enable = true,
     },
